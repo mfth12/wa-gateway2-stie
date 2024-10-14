2f9572fe70313d74069bd3428e0f2f24c0afb42e
Miftahul Haq <ciftah12@gmail.com>
2024-10-14 12:49:48 +0700

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'gateway2-multisession',
+      name: 'gateway2-multi',
       script: 'npm run start',
       watch: false,
       ignore_watch: ['node_modules', 'wa_credentials', 'public', '.git'],
